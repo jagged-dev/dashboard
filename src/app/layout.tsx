e5f09fc44786d75c4f3fa1793f822dd8d7aaad34
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-07 00:23:27 -0800

    Updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
 import "@/src/css/styles.css";
 import { inter } from "@/src/ts/fonts";
-// import { Metadata } from "next";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: {
-//     template: "%s | Acme Dashboard",
-//     default: "Acme Dashboard",
-//   },
-//   description: "The official Next.js Learn Dashboard built with App Router.",
-//   icons: {
-//     icon: "/favicon.ico",
-//   },
-//   openGraph: {
-//     title: "Acme Dashboard",
-//     description: "The official Next.js Learn Dashboard built with App Router.",
-//     images: [
-//       {
-//         url: "http://localhost:3000/opengraph-image.png?opengraph-image.4bef2aca.png",
-//         width: 1686,
-//         height: 882,
-//         type: "image/png",
-//       },
-//     ],
-//   },
-// };
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Acme Dashboard",
+    description: "The official Next.js Learn Dashboard built with App Router.",
+    images: [
+      {
+        url: "http://localhost:3000/opengraph-image.png?opengraph-image.4bef2aca.png",
+        width: 1686,
+        height: 882,
+        type: "image/png",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
