4de9a5e5749f3b5b544a9a104ed1a865ecb71257
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-06 23:12:24 -0800

    Updated file structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import "@/src/css/styles.css";
+import { inter } from "@/src/ts/fonts";
+import { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: {
+//     template: "%s | Acme Dashboard",
+//     default: "Acme Dashboard",
+//   },
+//   description: "The official Next.js Learn Dashboard built with App Router.",
+//   icons: {
+//     icon: "/favicon.ico",
+//   },
+//   openGraph: {
+//     title: "Acme Dashboard",
+//     description: "The official Next.js Learn Dashboard built with App Router.",
+//     images: [
+//       {
+//         url: "http://localhost:3000/opengraph-image.png?opengraph-image.4bef2aca.png",
+//         width: 1686,
+//         height: 882,
+//         type: "image/png",
+//       },
+//     ],
+//   },
+// };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
